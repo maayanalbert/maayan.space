9c0c2e0597f43e5279a6c957ef2cb550f4e3a1c4
maayanalbert <maayan.albert@gmail.com>
2024-05-24 17:59:40 -0400

    Update ContactInfo.tsx

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -6,20 +6,25 @@ import TextLink from "./TextLink"
 
 export default function ContactInfo() {
   return (
-    <div className="flex sm:flex-row flex-col gap-5">
+    <div className="flex sm:flex-row flex-col gap-5 sm:gap-10 sm:text-[18px] text-[16px]">
       <TextLink
         text="maayan@eve.space"
         href="mailto:maayan@eve.space"
         page="CONTACT"
       />
       <TextLink
-        text="twitter.com/_maayanster"
+        text="Twitter"
         href="https://twitter.com/_maayanster"
         page="CONTACT"
       />
       <TextLink
-        text="github.com/maayanalbert"
+        text="Github"
         href="https://github.com/maayanalbert"
+        page="CONTACT"
+      />
+      <TextLink
+        text="Linkedin"
+        href="https://www.linkedin.com/in/maayan-albert/"
         page="CONTACT"
       />
     </div>
