77e533f2a2d69e309f4be7a71fdb94dd1dd3624d
maayanalbert <maayan.albert@gmail.com>
2024-07-08 16:52:57 -0700

    use buzzwords
    Update DefaultInfo.tsx

--- src/components/DefaultInfo.tsx
+++ src/components/DefaultInfo.tsx
@@ -1,25 +1,10 @@
-import { useEffect } from "react"
-import NavButtons from "@/components/NavButtons"
-import { Item, Row } from "@/components/Item"
-import { getPageColor } from "@/pageHelpers"
 import TextLink from "./TextLink"
 
 export function DefaultInfo() {
   return (
     <p className="sm:text-[18px] text-[16px]">
-      I build tools for humans. Inspirations include
-      <TextLink
-        text="the Macintosh"
-        href="https://folklore.org/0-index.html"
-        page="DEFAULT"
-      />
-      and classic hits like the
-      <TextLink
-        text="paperclip and zipper."
-        href="https://www.penguinrandomhouse.com/books/130244/the-evolution-of-useful-things-by-henry-petroski/"
-        page="DEFAULT"
-      />{" "}
-      Currently building
+      I work at the intersection of design, technology, and the human
+      experience. Currently exploring AI personas for emotional support with
       <TextLink text="Eve." href="https://www.eve.space/" page="DEFAULT" />
     </p>
   )
